f795e7c1f18db9e7fab8efa1c246a3eaec2e9ed9
Rai4u <yevhenii.shevchenko@nure.ua>
2017-09-16 16:58:14 +0300

    Create new Class (InputAndOutputClasses) where i described some function which can take information from console and use this information for calculation. Rewrite Application class adding one function which is make work cyclic

--- calculator/src/main/java/com/rai4u/Application.java
+++ calculator/src/main/java/com/rai4u/Application.java
@@ -1,37 +1,36 @@
 package com.rai4u;
 
+import com.rai4u.InputAndOutputClasses.InputAndOutputData;
 import com.rai4u.sign.Operation;
-
-import java.math.BigDecimal;
-import java.util.Scanner;
 
 /**
  * Created by user on 15.09.2017.
  */
 
 public class Application {
-
     Application() {
 
-    }
-
-    void Start() {
-        String operation;
-        BigDecimal firstValue, secondValue;
-        Scanner scan = new Scanner(System.in);
-        firstValue = scan.nextBigDecimal();
-        operation = scan.next();
-        secondValue = scan.nextBigDecimal();
-        Operation forCalc = Operation.get(operation);
-        System.out.print(firstValue
-                + operation
-                + secondValue
-                + " = "
-                + forCalc.calculate(firstValue,secondValue));
     }
 
     public static void main(String[] args) {
         Application app = new Application();
         app.Start();
+    }
+
+    private void createUI(InputAndOutputData Data) {
+        do {
+            Data.readData();
+            Operation forCalc = Operation.get(Data.getTypeOfOperation());
+            if(forCalc == null){
+                break;
+            }
+            Data.printFinalExample(forCalc);
+            Data.exitCondition();
+        } while (Data.getExitFromApp() == 1);
+    }
+
+    void Start() {
+        InputAndOutputData Data = new InputAndOutputData();
+        createUI(Data);
     }
 }
