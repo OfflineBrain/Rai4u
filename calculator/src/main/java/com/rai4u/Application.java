5c41e83b199e53ed7337380cb406ab1e0ced937a
Rai4u <yevhenii.shevchenko@nure.ua>
2017-09-16 13:34:12 +0300

    Create app which used my enum like operation for calculator

--- calculator/src/main/java/com/rai4u/Application.java
+++ calculator/src/main/java/com/rai4u/Application.java
@@ -1,5 +1,10 @@
 package com.rai4u;
 
+import com.rai4u.sign.Operation;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 /**
  * Created by user on 15.09.2017.
  */
@@ -11,7 +16,18 @@ public class Application {
     }
 
     void Start() {
-
+        String operation;
+        BigDecimal firstValue, secondValue;
+        Scanner scan = new Scanner(System.in);
+        firstValue = scan.nextBigDecimal();
+        operation = scan.next();
+        secondValue = scan.nextBigDecimal();
+        Operation forCalc = Operation.get(operation);
+        System.out.print(firstValue
+                + operation
+                + secondValue
+                + " = "
+                + forCalc.calculate(firstValue,secondValue));
     }
 
     public static void main(String[] args) {
